dd731974c1aaa6c5e0bc0e773d1fa56dc35b3aae
mjwakex <mjwakex@hotmail.com>
2025-06-13 13:17:01 +0200

    priting out stint data. Renamed column "LapNumber" to "StintLength"

--- src/main.py
+++ src/main.py
@@ -24,19 +24,28 @@ laps_22 = session_22.laps
 laps_23 = session_23.laps
 laps_24 = session_24.laps
 
-# getting the stint data for each driver trhoughout the race
+# getting the stint data (stint num, tyre compound, stint length) for each driver throughout each race
 stints_21 = laps_21[["Driver", "Stint", "Compound", "LapNumber"]]
 stints_21 = stints_21.groupby(["Driver", "Stint", "Compound"])
 stints_21 = stints_21.count().reset_index()
+stints_21 = stints_21.rename(columns={"LapNumber": "StintLength"})
 
 stints_22 = laps_22[["Driver", "Stint", "Compound", "LapNumber"]]
 stints_22 = stints_22.groupby(["Driver", "Stint", "Compound"])
 stints_22 = stints_22.count().reset_index()
+stints_22 = stints_22.rename(columns={"LapNumber": "StintLength"})
 
 stints_23 = laps_23[["Driver", "Stint", "Compound", "LapNumber"]]
 stints_23 = stints_23.groupby(["Driver", "Stint", "Compound"])
 stints_23 = stints_23.count().reset_index()
+stints_23 = stints_23.rename(columns={"LapNumber": "StintLength"})
 
 stints_24 = laps_24[["Driver", "Stint", "Compound", "LapNumber"]]
 stints_24 = stints_24.groupby(["Driver", "Stint", "Compound"])
 stints_24 = stints_24.count().reset_index()
+stints_24 = stints_24.rename(columns={"LapNumber": "StintLength"})
+
+list_of_stints = [stints_21, stints_22, stints_23, stints_24]
+
+for stints in list_of_stints:
+    print(stints)
