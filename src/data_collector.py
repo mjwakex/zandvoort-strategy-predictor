768b7e661b03486c21a35fdc421c01b2eaa82348
mjwakex <mjwakex@hotmail.com>
2025-06-15 21:36:14 +0100

    renamed file from "main" to "data_collector" and added new column to csv

---
+++ src/data_collector.py
@@ -0,0 +1,67 @@
+'''
+A pit stop predictor for the dutch gp
+'''
+
+import fastf1
+from fastf1 import plotting
+
+# cache directory
+fastf1.Cache.enable_cache('../cache')
+
+# we want the data from the races of the past 4 (2021) years since it was reintroduced to calendar
+session_21 = fastf1.get_session(2021, "Netherlands", "R")
+session_22 = fastf1.get_session(2022, "Netherlands", "R")
+session_23 = fastf1.get_session(2023, "Netherlands", "R")
+session_24 = fastf1.get_session(2024, "Netherlands", "R")
+
+session_21.load()
+session_22.load()
+session_23.load()
+session_24.load()
+
+laps_21 = session_21.laps
+laps_22 = session_22.laps
+laps_23 = session_23.laps
+laps_24 = session_24.laps
+
+# getting the stint data (stint num, tyre compound, stint length) for each driver throughout each race
+stints_21 = laps_21[["Driver", "Stint", "Compound", "LapNumber"]]
+stints_21 = stints_21.groupby(["Driver", "Stint", "Compound"]).agg(
+    PitLap = ("LapNumber", 'max'),
+    StintLength = ("LapNumber", 'size')
+).reset_index()
+
+stints_22 = laps_22[["Driver", "Stint", "Compound", "LapNumber"]]
+stints_22 = stints_22.groupby(["Driver", "Stint", "Compound"]).agg(
+    PitLap = ("LapNumber", 'max'),
+    StintLength = ("LapNumber", 'size')
+).reset_index()
+
+stints_23 = laps_23[["Driver", "Stint", "Compound", "LapNumber"]]
+stints_23 = stints_23.groupby(["Driver", "Stint", "Compound"]).agg(
+    PitLap = ("LapNumber", 'max'),
+    StintLength = ("LapNumber", 'size')
+).reset_index()
+
+stints_24 = laps_24[["Driver", "Stint", "Compound", "LapNumber"]]
+stints_24 = stints_24.groupby(["Driver", "Stint", "Compound"]).agg(
+    PitLap = ("LapNumber", 'max'),
+    StintLength = ("LapNumber", 'size')
+).reset_index()
+
+# print out stitn data from each loaded session
+list_of_stints = [stints_21, stints_22, stints_23, stints_24]
+
+def write_to_dataset(stint, filename="../data/dataset.csv", mode="a"):
+    try: 
+        stint.to_csv(filename, mode=mode, header=(mode=="w"), index=False)
+    except Exception as e:
+        print(f"Error writing to dataset: {e}")
+
+
+for i, stints in enumerate(list_of_stints):
+    print(stints)
+    mode = "w" if i == 0 else "a"
+    write_to_dataset(stints, mode=mode)
+
+
