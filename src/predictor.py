c6dd6a1bead24d9acc6c9d4b2bc488fd7ae008fe
mjwakex <mjwakex@hotmail.com>
2025-06-17 11:00:21 +0100

    added total number of laps for race, made input case-insensitive, and added output of pitlap and stint length. Output is FIN if stint goes to end of race

--- src/predictor.py
+++ src/predictor.py
@@ -1,15 +1,18 @@
 import pandas as pd
 from collections import Counter
 
+# number of laps for dutch gp 
+LAPS = 71
+
 # read the dataset
 df = pd.read_csv("../data/dataset.csv")
-# print(df.head())
+
 
 # get the starting compound
 valid = False
 compounds = ["SOFT", "MEDIUM", "HARD", "INTERMEDIATE", "WET"]
 while not valid:
-    starting_compound = input("Starting Compound: ")
+    starting_compound = input("Starting Compound: ").upper()
     if starting_compound in compounds:
         valid = True
 
@@ -21,23 +24,31 @@ stint_1 = df[df["Stint"] == 1.0]
 
 
 # get all previoud strategies by driver
-# print("Total races in raw data:", len(df.groupby(['Year', 'Driver'])))
-strategies = df.groupby(['Year', 'Driver']).agg(list).reset_index()
-# print("Total rows in strategy list:", len(strategies))
+# driver_strategies = df.groupby(['Year', 'Driver']).agg(list).reset_index()
+
+strategies = []
+
+for (year, driver), group in df.groupby(['Year', 'Driver']):
+    # Sort by stint within each group
+    group = group.sort_values(by='Stint')
+    compound_list = group['Compound'].tolist()
+    pitlap_list = group['PitLap'].tolist()
+    length_list = group['StintLength'].tolist()
+
+    # filtering based on starting compound
+    if compound_list and compound_list[0] == starting_compound:
+        strategy = list(zip(compound_list, pitlap_list, length_list))
+        strategies.append(strategy)
 
 
-# filter strategies based on starting compound
-filtered_strats = strategies[strategies["Compound"].str[0] == starting_compound]
 
-
-# find most common second and third stints
-compound_strats = []
-for stints in filtered_strats["Compound"]:
-    compound_strats.append(tuple(stints))
-
-strategy_counts = Counter(compound_strats)
+# count most common strategy
+strategy_counts = Counter(tuple(s) for s in strategies)
 most_common = strategy_counts.most_common(1)[0][0]
 
+# output result
 print("\nFull strategy breakdown:")
-for i, compound in enumerate(most_common):
-    print(f"Stint {i + 1}: {compound}")
+for i, (compound, pitlap, stint_len) in enumerate(most_common):
+    # replace pit lap with FIN if last lap 
+    pitlap_display = "FIN" if int(pitlap) == LAPS else int(pitlap)
+    print(f"Stint {i + 1}: {compound}, pit lap = {pitlap_display}, stint length = {int(stint_len)}")
